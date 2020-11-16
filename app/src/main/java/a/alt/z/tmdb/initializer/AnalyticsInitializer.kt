91ec5fb72ef907f2fdf10f29b982efaabb07f5c4
lee-dev-git <lee-dev@kakao.com>
2020-11-16 14:32:52 +0900

    tmdb-hilt

---
+++ app/src/main/java/a/alt/z/tmdb/initializer/AnalyticsInitializer.kt
@@ -0,0 +1,4 @@
+package a.alt.z.tmdb.initializer
+
+class AnalyticsInitializer {
+}
---
+++ app/src/main/java/a/alt/z/tmdb/initializer/TimberInitializer.kt
@@ -0,0 +1,13 @@
+package a.alt.z.tmdb.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+
+class TimberInitializer: Initializer<Unit> {
+
+    override fun create(context: Context) {
+        TODO("Not yet implemented")
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+}
